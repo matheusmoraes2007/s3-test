c00bb80dff4108eb04ee66df18b6928ed2a4e244
matheusmoraes2007 <matheus.moraes@3035tech.com>
2025-09-03 08:51:17 -0300

    add: Post method for files

---
+++ src/main/java/com/tech/s3test/controller/FileController.java
@@ -0,0 +1,28 @@
+package com.tech.s3test.controller;
+
+import com.tech.s3test.dto.req.fileReqDto;
+import com.tech.s3test.service.FileService;
+import com.tech.s3test.utils.ControllerUtils;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/file")
+@Slf4j
+@RequiredArgsConstructor
+public class FileController {
+    private final ControllerUtils controllerUtils;
+    private final FileService fileService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void upload(@Valid @RequestParam("file") fileReqDto file) {
+        String requestId = controllerUtils.generateUUID();
+        log.info("ID[{}] Request file name '{}' received", requestId, file.file().getOriginalFilename());
+        fileService.saveFile(file, requestId);
+    }
+}
