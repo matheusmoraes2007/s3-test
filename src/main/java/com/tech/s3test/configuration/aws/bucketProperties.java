d0620988698962ac6012fc3cddf0ca5a5b2c36ab
matheusmoraes2007 <matheus.moraes@3035tech.com>
2025-09-03 11:36:24 -0300

    add: mapped variables

---
+++ src/main/java/com/tech/s3test/configuration/aws/bucketProperties.java
@@ -0,0 +1,17 @@
+package com.tech.s3test.configuration.aws;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.yml")
+@ConfigurationProperties(prefix = "aws")
+@Data
+public class bucketProperties {
+    private String bucketName;
+    private String region;
+    private String secretKey;
+    private String accessKey;
+}
