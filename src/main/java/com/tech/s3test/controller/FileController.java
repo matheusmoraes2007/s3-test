a130deb056dae431ace49881beb9d83cce4b97c8
matheusmoraes2007 <matheus.moraes@3035tech.com>
2025-09-03 11:20:43 -0300

    remove: dto unused

--- src/main/java/com/tech/s3test/controller/FileController.java
+++ src/main/java/com/tech/s3test/controller/FileController.java
@@ -1,12 +1,12 @@
 package com.tech.s3test.controller;
 
-import com.tech.s3test.dto.req.fileReqDto;
 import com.tech.s3test.service.FileService;
 import com.tech.s3test.utils.ControllerUtils;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,11 +18,11 @@ public class FileController {
     private final ControllerUtils controllerUtils;
     private final FileService fileService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public void upload(@Valid @RequestParam("file") fileReqDto file) {
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> upload(@RequestPart("file") MultipartFile file) {
         String requestId = controllerUtils.generateUUID();
         log.info("ID[{}] Request received", requestId);
         fileService.saveFile(file, requestId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
