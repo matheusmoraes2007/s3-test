4d36bb76d2ff07b0b7e74762ad0f1e1cc33cdcd5
matheusmoraes2007 <matheus.moraes@3035tech.com>
2025-09-04 09:02:43 -0300

    add: download file

--- src/main/java/com/tech/s3test/controller/FileController.java
+++ src/main/java/com/tech/s3test/controller/FileController.java
@@ -1,9 +1,10 @@
 package com.tech.s3test.controller;
 
+import com.tech.s3test.dto.req.FileResDto;
 import com.tech.s3test.dto.res.SaveFileResDto;
 import com.tech.s3test.service.FileService;
-import com.tech.s3test.utils.ControllerUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,6 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/file")
 @RequiredArgsConstructor
 public class FileController {
-    private final ControllerUtils controllerUtils;
     private final FileService fileService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -22,5 +22,15 @@ public class FileController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(fileService.saveFile(file));
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<byte[]> download(@PathVariable("fileName") String fileName) {
+        FileResDto file = fileService.downloadFile(fileName);
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.parseMediaType(file.contentType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(file.file());
     }
 }
