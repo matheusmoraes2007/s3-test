e595ae47fae398ce908b435022dc235f79e22372
matheusmoraes2007 <matheus.moraes@3035tech.com>
2025-09-04 09:01:06 -0300

    add: method of contacting s3

--- src/main/java/com/tech/s3test/service/S3Service.java
+++ src/main/java/com/tech/s3test/service/S3Service.java
@@ -1,12 +1,14 @@
 package com.tech.s3test.service;
 
 import com.tech.s3test.configuration.aws.BucketProperties;
+import com.tech.s3test.dto.req.FileResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.*;
 
 import java.io.IOException;
 
@@ -24,6 +26,22 @@ public class S3Service {
                     .contentType(file.getContentType())
                     .build();
             s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public FileResDto download(String fileName) {
+        try {
+            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                    .bucket(bucketProperties.getBucketName())
+                    .key(fileName)
+                    .build();
+            ResponseInputStream<GetObjectResponse> s3Object = s3Client.getObject(getObjectRequest);
+            return new FileResDto(
+                    s3Object.readAllBytes(),
+                    s3Object.response().contentType()
+            );
         } catch (IOException e) {
             throw new RuntimeException(e.getMessage());
         }
