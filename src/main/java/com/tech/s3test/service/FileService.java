ea81a6589f5102182629681ce905719a87aec1f3
matheusmoraes2007 <matheus.moraes@3035tech.com>
2025-09-05 10:53:18 -0300

    remove: log

--- src/main/java/com/tech/s3test/service/FileService.java
+++ src/main/java/com/tech/s3test/service/FileService.java
@@ -5,7 +5,6 @@ import com.tech.s3test.dto.res.FileResDto;
 import com.tech.s3test.dto.res.SaveFileResDto;
 import com.tech.s3test.exception.custom.MissingStatementException;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,19 +12,15 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class FileService {
     private final StoragePort storagePort;
 
     public SaveFileResDto save(MultipartFile file) {
-        log.info("Starting save process");
         String originalFileName = file.getOriginalFilename();
         this.validateFileName(originalFileName);
         String fileExtension = originalFileName.substring(file.getOriginalFilename().lastIndexOf("."));
         String key = UUID.randomUUID() + fileExtension;
-        log.debug("Unique key generated: {}", key);
         storagePort.save(key, file);
-        log.info("File has been saved successfully, key: {}", key);
         return new SaveFileResDto(key);
     }
 
@@ -34,15 +29,11 @@ public class FileService {
     }
 
     public void putUpdateByKey(String key, MultipartFile file) {
-        log.info("Starting update process, key: {}", key);
         storagePort.update(key, file);
-        log.info("File has been updated successfully, key: {}", key);
     }
 
     public void deleteByKey(String key) {
-        log.info("Starting delete process, key: {}", key);
         storagePort.delete(key);
-        log.info("File has been deleted successfully, key: {}", key);
     }
 
     private void validateFileName(String fileName) {
