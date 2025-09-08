34b2c8484ab426ed9ce502e6c995837b2889dd46
Matheus Moraes <matheus.moraes@3035tech.com>
2025-09-08 12:39:01 -0300

    add: user repository

---
+++ src/main/java/com/tech/s3test/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tech.s3test.repository;
+
+import com.tech.s3test.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
