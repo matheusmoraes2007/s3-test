99a62b83ba5e3cf22464c494ff37d00a8b51a021
Matheus Moraes <matheus.moraes@3035tech.com>
2025-09-08 10:45:47 -0300

    fix: removed ununuse annotation

--- src/main/java/com/tech/s3test/configuration/aws/S3Configuration.java
+++ src/main/java/com/tech/s3test/configuration/aws/S3Configuration.java
@@ -1,12 +1,10 @@
 package com.tech.s3test.configuration.aws;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-@RequiredArgsConstructor
 public class S3Configuration {
 
     @Bean
